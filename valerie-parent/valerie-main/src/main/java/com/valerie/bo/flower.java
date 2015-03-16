67efb3c2d10f4a4633b0af4514430c0d5cda9e2e
ValerieXi <lyvalley@gmail.com>
2015-03-16 19:45:41 +0800

    init

---
+++ valerie-parent/valerie-main/src/main/java/com/valerie/bo/flower.java
@@ -0,0 +1,10 @@
+package com.valerie.bo;
+
+/**
+ * Created by valerie on 15/3/16
+ */
+public class flower {
+    public static void  main (String[] args){
+        System.out.print("hello valerie");
+    }
+}
---
+++ valerie-parent/valerie-main/src/main/java/com/valerie/vo/moon.java
@@ -0,0 +1,7 @@
+package com.valerie.vo;
+
+/**
+ * Created by valerie on 15/3/16
+ */
+public class moon {
+}
